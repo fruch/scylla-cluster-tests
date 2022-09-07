ba290a78cead792e98dd72225e1ff43313f1687f
Israel Fruchter <fruch@scylladb.com>
2022-09-07 23:47:57 +0300

    refactor(test_utils_data_validator): switch to pytest style
    * created fixture `fixture_params` that can be later move to
      `conftest.py` and be share with more tests
    
    * removed all the redudent classes of unittest style

--- unit_tests/test_utils_data_validator.py
+++ unit_tests/test_utils_data_validator.py
@@ -10,85 +10,59 @@
 # See LICENSE for more details.
 #
 # Copyright (c) 2022 ScyllaDB
-import logging
 import os
-import unittest
+from dataclasses import dataclass
+
+import pytest
 
 from sdcm.utils.data_validator import LongevityDataValidator
 from sdcm import sct_config
 
 
-class ConfigurationBase(unittest.TestCase):  # pylint: disable=too-many-public-methods
-    _testMethodName = "runTest"
+@pytest.fixture(scope="function", name="params")
+def fixture_params(request: pytest.FixtureRequest):
+    config_files = request.node.get_closest_marker("sct_config").kwargs.get('files')
+    if config_files:
+        os.environ['SCT_CONFIG_FILES'] = config_files
 
-    @classmethod
-    def setUpClass(cls):
-        logging.basicConfig(level=logging.ERROR)
-        logging.getLogger('botocore').setLevel(logging.CRITICAL)
-        logging.getLogger('boto3').setLevel(logging.CRITICAL)
-        logging.getLogger('anyconfig').setLevel(logging.ERROR)
+    os.environ['SCT_CLUSTER_BACKEND'] = 'aws'
+    os.environ['SCT_AMI_ID_DB_SCYLLA'] = 'ami-06f919eb'
+    os.environ['SCT_INSTANCE_TYPE_DB'] = 'i3.large'
+    params = sct_config.SCTConfiguration()  # pylint: disable=attribute-defined-outside-init
+    params.verify_configuration()
 
-        cls.setup_default_env()
+    yield params
 
-        os.environ['SCT_CLUSTER_BACKEND'] = 'aws'
-        os.environ['SCT_AMI_ID_DB_SCYLLA'] = 'ami-06f919eb'
-        cls.conf = sct_config.SCTConfiguration()
-
-        cls.clear_sct_env_variables()
-
-        # some of the tests assume this basic case is define, to avoid putting this again and again in each test
-        # and so we can run those tests specifically
-        cls.setup_default_env()
-
-    def tearDown(self):
-        self.clear_sct_env_variables()
-        self.setup_default_env()
-
-    @classmethod
-    def setup_default_env(cls):
-        os.environ['SCT_CONFIG_FILES'] = 'internal_test_data/minimal_test_case.yaml'
-
-    @classmethod
-    def clear_sct_env_variables(cls):
-        for k in os.environ:
-            if k.startswith('SCT_'):
-                del os.environ[k]
+    for k in os.environ:
+        if k.startswith('SCT_'):
+            del os.environ[k]
 
 
-class TestDataValidator(ConfigurationBase):
-    def test_view_names_for_updated_data(self):
-        os.environ[
-            'SCT_CONFIG_FILES'] = 'unit_tests/test_data/test_data_validator/lwt-basic-3h.yaml'
-        os.environ['SCT_CLUSTER_BACKEND'] = 'aws'
-        os.environ['SCT_AMI_ID_DB_SCYLLA'] = 'ami-06f919eb'
-        os.environ['SCT_INSTANCE_TYPE_DB'] = 'i3.large'
-        self.params = sct_config.SCTConfiguration()  # pylint: disable=attribute-defined-outside-init
-        self.params.verify_configuration()
+@dataclass
+class MockLongevityTest:
+    params: sct_config.SCTConfiguration
 
-        data_validator = LongevityDataValidator(longevity_self_object=self,
-                                                user_profile_name='c-s_lwt',
-                                                base_table_partition_keys=['domain', 'published_date'])
-        data_validator._validate_updated_per_view = [True, True]   # pylint: disable=protected-access
-        views_list = data_validator.list_of_view_names_for_update_test()
-        assert views_list == [('blogposts_update_one_column_lwt_indicator',
-                               'blogposts_update_one_column_lwt_indicator_after_update',
-                               'blogposts_update_one_column_lwt_indicator_expect', True),
-                              ('blogposts_update_2_columns_lwt_indicator',
-                               'blogposts_update_2_columns_lwt_indicator_after_update',
-                               'blogposts_update_2_columns_lwt_indicator_expect', True)]
 
-    def test_view_names_for_updated_data_not_found(self):
-        os.environ[
-            'SCT_CONFIG_FILES'] = 'unit_tests/test_data/test_data_validator/no-validation-views-lwt-basic-3h.yaml'
-        os.environ['SCT_CLUSTER_BACKEND'] = 'aws'
-        os.environ['SCT_AMI_ID_DB_SCYLLA'] = 'ami-06f919eb'
-        os.environ['SCT_INSTANCE_TYPE_DB'] = 'i3.large'
-        self.params = sct_config.SCTConfiguration()  # pylint: disable=attribute-defined-outside-init
-        self.params.verify_configuration()
+@pytest.mark.sct_config(files='unit_tests/test_data/test_data_validator/lwt-basic-3h.yaml')
+def test_view_names_for_updated_data(params):
+    data_validator = LongevityDataValidator(longevity_self_object=MockLongevityTest(params=params),
+                                            user_profile_name='c-s_lwt',
+                                            base_table_partition_keys=['domain', 'published_date'])
+    data_validator._validate_updated_per_view = [True, True]   # pylint: disable=protected-access
+    views_list = data_validator.list_of_view_names_for_update_test()
+    assert views_list == [('blogposts_update_one_column_lwt_indicator',
+                           'blogposts_update_one_column_lwt_indicator_after_update',
+                           'blogposts_update_one_column_lwt_indicator_expect', True),
+                          ('blogposts_update_2_columns_lwt_indicator',
+                           'blogposts_update_2_columns_lwt_indicator_after_update',
+                           'blogposts_update_2_columns_lwt_indicator_expect', True)]
 
-        data_validator = LongevityDataValidator(longevity_self_object=self,
-                                                user_profile_name='c-s_lwt',
-                                                base_table_partition_keys=['domain', 'published_date'])
-        data_validator._validate_updated_per_view = [True, True]   # pylint: disable=protected-access
-        views_list = data_validator.list_of_view_names_for_update_test()
-        assert views_list == []
+
+@pytest.mark.sct_config(files='unit_tests/test_data/test_data_validator/no-validation-views-lwt-basic-3h.yaml')
+def test_view_names_for_updated_data_not_found(params):
+    data_validator = LongevityDataValidator(longevity_self_object=MockLongevityTest(params=params),
+                                            user_profile_name='c-s_lwt',
+                                            base_table_partition_keys=['domain', 'published_date'])
+    data_validator._validate_updated_per_view = [True, True]   # pylint: disable=protected-access
+    views_list = data_validator.list_of_view_names_for_update_test()
+    assert views_list == []
